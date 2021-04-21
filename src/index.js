712b31a253c90d511c76b9f4bfc66c781f652983
Jef Schoolmeester <73157784+Jef-io@users.noreply.github.com>
2021-04-21 13:17:39 +0200

    [FEAT]
    pages and css layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+let competences = ['Web Apps', 'Websites', 'Mobile Apps', 'API\'s'];
+let competenceId = 0;
+
+// const competenceTransition = (id) => {
+//     for (let i = 0; i < 8; i++) {
+
+//     }
+// }
+
+setInterval(() => {
+    document.getElementById('competences').innerHTML = competences[competenceId];
+    competenceId = (competenceId + 1) %4;
+}, 5000)
